8d045ccd8f8dcbbc495c86102f839421c2d65452
Alexandre Hary <alexandre.harry10@gmail.com>
2023-08-15 18:43:59 +0200

    rail task manager done, delete pb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # get 'tasks', to: 'tasks#index', as: :tasks # liste des task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task # add new
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id', to: 'tasks#show', as: :task # redirection via click sur titre
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task # as: :edit_task possible
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :destroy_task
+  resources :tasks, except: [:destroy]
 end
